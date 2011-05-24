518ae34a284dc68abb5c2ff470f552bd827a84a9
Felix Geisendörfer <felix@debuggable.com>
2011-05-24 16:14:23 +0200

    Delete more code that is not being used

--- js/map.js
+++ js/map.js
@@ -1,37 +1,77 @@
 /**
- * Map Test 1
+ * Greenpeace Map Application
  * @author rbertot@greenpeace.org
  * @copyright 2011 Greenpeace International
  *
  * See "MAP Web Author JavaScript API" at
  * http://www.avenza.com/mapublisher/mapwebauthor/api
  */
-jQuery(function($) {
-  var theMap = AVENZA.embedViewer(
-    "map", // div#id
-    "950", // Width
-    "600", // Height
-    {
-      baseURL:"/map/swf/",
-      initialZoom:200,
-      initialPanX:200,
-      initialPanY:600,
-      useCentroidForCallouts:true,
-      panWidget: false,
-      zoomWidget: true,
-      searchWidget: false,
-      layerWidget: true,
-      overviewWidget: false,
-      zoomMin:200,
-      zoomMax:5000,
-      flashSecuritySandbox: AVENZA.AUTO_SANDBOX
+theMap = AVENZA.embedViewer(
+  'map','960','450',
+  {
+    baseURL:'swf',
+    initialZoom:100,
+    initialPanX:000,
+    initialPanY:100,
+    useCentroidForCallouts:true,
+    panWidget: false,
+    zoomWidget: true,
+    searchWidget: false,
+    layerWidget: false,
+    overviewWidget: false,
+    zoomMin:150,
+    zoomMax:3000,
+    flashSecuritySandbox: AVENZA.AUTO_SANDBOX,
+    loadedCB: function () {
+      init();
+    }
+  }
+);
+    
+function init() {
+  var m = theMap.element();
+  var show = true;
+  if(m) {
+    // Locations;
+    $('.main.nav .home').click(function() {
+      m.panHome();
+    });
+    $('.main.nav #sumatra').click(function() {
+      m.panToPointAndZoom(0, 250, 200)
+    });
+    if (window.addEventListener) {
+      m.addEventListener("mousemove", onMouseMove, false);
     } 
-  );
-  $('#map-nav #home').click(function() {
-    theMap.panHome();
-  });
+    else if (window.attachEvent) { // IE
+      m.attachEvent("onmousemove", onMouseMove);
+    }
+    // Layers
+    $('#orangutan').click(function() {
+      //m.panHome();
+      show = !show;
+      theMap.setVisible('Orangutan', show);
+    });
 
-  $('#map-nav #indonesia').click(function() {
-    theMap.panToPointAndZoom(500, 450, 900)
-  });
-});
+  }
+}
+
+function onMouseMove (event) {
+  var f = theMap.retrieve(AVENZA.FEATURE);
+  //var f = theMap.retrieve(AVENZA.ATTRIBUTES);
+  //alert(f.attributes.name);
+
+  if (f.attributes) {
+    $("#popup").css("top", event.pageY+20);
+    $("#popup").css("left", event.pageX+20);
+    $("#popup").css("visibility", "visible");
+    $("#popup .name").html(f.attributes.name);   
+    $("#popup .stopid").html(f.attributes.stopid);
+    $("#popup .latitude").html(f.attributes.latitude);
+    $("#popup .longitude").html(f.attributes.longitude);
+    // $("#popup .accesible").html(f.attributes.accessible);
+    // $("#popup .sundayonly").html(f.attributes.sundayonly);
+    // $("#popup .trapezeid").html(f.attributes.trapezeid); 
+  } else {
+    $("#popup").css("visibility", "hidden");
+  }
+}
