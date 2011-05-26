3ed14aa20d7477fc8d850806e286e22b1d551743
Felix Geisendörfer <felix@debuggable.com>
2011-05-26 19:50:04 +0200

    Split up AvenzaMap / AvenzaLayer

---
+++ js/lib/avenza_layer.js
@@ -0,0 +1,65 @@
+function AvenzaLayer() {
+  this.map = null;
+  this.xmlElement = null;
+
+  this.alpha = 100;
+  this.name = null;
+  this.visible = null;
+  this.highlighted = false;
+}
+
+AvenzaLayer.create = function(options) {
+  var avenzaLayer = new AvenzaLayer();
+
+  $.extend(avenzaLayer, options);
+  avenzaLayer._initialize();
+
+  return avenzaLayer;
+};
+
+AvenzaLayer.prototype._initialize = function() {
+  this.name = $(this.xmlElement).attr('name');
+  this.visible = $(this.xmlElement).attr('visible') === 'true';
+};
+
+AvenzaLayer.prototype.hide = function() {
+  this.visible = false;
+  this._updateVisibility();
+};
+
+AvenzaLayer.prototype.show = function() {
+  this.visible = true;
+  this._updateVisibility();
+};
+
+AvenzaLayer.prototype.toggle = function() {
+  this.visible = !this.visible;
+  this._updateVisibility();
+};
+
+AvenzaLayer.prototype.toggleHighlight = function() {
+  var layers = this.map.getLayers();
+  var self = this;
+
+  this.highlighted = !this.highlighted;
+
+  $(layers).each(function() {
+    if (!self.highlighted && this !== self) {
+      this.setAlpha(1);
+      this._updateVisibility();
+    }
+
+    if (self.highlighted && this !== self) {
+      this.setAlpha(0.1);
+    }
+  });
+};
+
+AvenzaLayer.prototype.setAlpha = function(alpha) {
+  this.alpha = alpha;
+  this.map.map.setAlpha(this.name, this.alpha);
+};
+
+AvenzaLayer.prototype._updateVisibility = function() {
+  this.map.map.setVisible(this.name, this.visible);
+};
