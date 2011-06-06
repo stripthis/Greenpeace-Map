b5910f6aa403cd183184ff3e20ba103680def2a0
stripthis <remy@stripthis.com>
2011-06-06 11:07:42 +0200

    ftp merge

---
+++ eng/risks.php
@@ -0,0 +1,21 @@
+  <!-- flickr slideshow -->
+<div id="flickr-holder">
+  <div id="flickr-text">
+    <p>Indonesia's forests and peatlands are critical for biodiversity, people and the climate. </p>
+    <p>Millions of hectares of Indonesia's rainforest habitats and carbon-rich peatlands remain at risk from clearance for pulp and palm oil plantations.</p>
+  </div>
+
+ <div id="flickr-slideshow">
+    <p><object width="740" height="455"> <param name="flashvars" value="offsite=true&lang=en-us&page_show_url=%2Fphotos%2Fgreenpeaceinternational%2Fsets%2F72157626758333245%2Fshow%2F&page_show_back_url=%2Fphotos%2Fgreenpeaceinternational%2Fsets%2F72157626758333245%2F&set_id=72157626758333245&jump_to="></param> <param name="movie" value="http://www.flickr.com/apps/slideshow/show.swf?v=104087"></param> <param name="allowFullScreen" value="true"></param><embed type="application/x-shockwave-flash" src="http://www.flickr.com/apps/slideshow/show.swf?v=104087" allowFullScreen="true" flashvars="offsite=true&lang=en-us&page_show_url=%2Fphotos%2Fgreenpeaceinternational%2Fsets%2F72157626758333245%2Fshow%2F&page_show_back_url=%2Fphotos%2Fgreenpeaceinternational%2Fsets%2F72157626758333245%2F&set_id=72157626758333245&jump_to=" width="740" height="455"></embed></object>
+    </p>
+  </div> 
+</div> 
+
+<div class="content-holder">
+  <div class="pages">
+    <div class="page plannedloss">
+<?php require('content/risks/plannedloss.html'); ?>
+    </div>
+  </div>
+</div>
+
